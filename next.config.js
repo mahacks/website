f8c70acfd7268cd31cfede9974ea985f736f5bb9
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-01-23 14:32:52 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = withMDX({
         source: '/prospectus',
         destination: '/MAHacks_VI_Sponsorship_Prospectus_20220122.pdf',
         permanent: false,
-
+      }, {
         source: '/poster',
         destination: '/MAHacks_VI_Attendee_Poster_20220123_1.pdf',
         permanent: false,
